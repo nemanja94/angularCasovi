7d938fdb7d47a34737cce43786d0bcc9eff7e925
Nemanja Andric <nemanja@localhost.fedora>
2020-02-18 09:37:01 -0500

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Article } from './components/article/article.model';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloWorld';
+	title = 'angular-redit app';
+	
+	articles: Article[] = new Array<Article>()
+
+	constructor() {
+		this.articles = [
+			new Article('IT255 - Veb sistemi 1', 'http://www.metropolitan.ac.rs', 3),
+			new Article('Fullstack', 'http://fullstack.io', 2),
+			new Article('Angular Homepage', 'http://angular.io', 1)
+		]
+	}
+
+	addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+		console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+		this.articles.push(new Article(title.value, link.value, 0));
+		title.value = '';
+		link.value = '';
+		return false;
+	}
 }
